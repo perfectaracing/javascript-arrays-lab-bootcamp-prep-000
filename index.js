562a6029fceea132249ca62981aa1ddcaefb8769
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-10 18:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function destructivelyRemoveLastKitten(name) {
   kittens.pop(name);
   return kittens;
 }
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
